8899ef2ce7ca0f5e596b2f6ab1e31506d9714480
antedeguemon <antedeguemon@users.noreply.github.com>
2021-12-12 16:11:13 -0500

    New site

--- lib/mix/tasks/cyberspace/write.ex
+++
@@ -1,8 +0,0 @@
-defmodule Mix.Tasks.Cyberspace.Write do
-  use Mix.Task
-
-  def run([output_path]) do
-    rendered_html = Cyberspace.render!()
-    File.write!(output_path, rendered_html)
-  end
-end
